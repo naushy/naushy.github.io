c594e2b0843b2754d654695843ea5fc477cf6c13
Naushy <naushy90@gmail.com>
2020-02-10 18:16:47 +0530

    grtool
    grtool

---
+++ grtool/brfv5-browser-master/js/utils/utils__colors.js
@@ -0,0 +1,6 @@
+export const colorPrimary   = '#ffdf00'
+export const colorSecondary = '#ffffff'
+export const colorWhitePale = 'rgb(255, 255, 255, 0.4)'
+export const colorOrange    = '#ff7900'
+
+export default { colorPrimary, colorSecondary, colorWhitePale, colorOrange }
